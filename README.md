f3c368943e7a617bae56ab7d26531439167cb4d3
Torbuntu <sorensentor@tuta.io>
2020-03-28 15:11:51 -0500

    adding collision. update readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Circus Pokitto game
+
+## TODO: 
+
+- x travel for jumper based on bar landing zone
+- display score
+- display lives
+- add title menu
+- music/sfx
--- main.cpp
+++ main.cpp
@@ -12,7 +12,13 @@ struct Boon{
     int x;
     int y;
     int color;
+    bool pop;
 };
+
+
+bool collides(int x, int y, Boon &row){
+    return (x < row.x + 8) && (x + 16 > row.x) && (y < row.y + 8) && (y + 16 > row.y);
+}
 
 int main(){
     using Pokitto::Core;
@@ -30,6 +36,8 @@ int main(){
     
     int rowSize = 19;
     
+    int score = 0;
+    
     Boon rowA[rowSize];
     Boon rowB[rowSize];
     Boon rowC[rowSize];
@@ -39,16 +47,19 @@ int main(){
         a.x = i*12;
         a.y = 16;
         a.color = 0;
+        a.pop = false;
         
         Boon b;
         b.x = i*12;
         b.y = 26;
         b.color = 16;
+        b.pop = false;
         
         Boon c;
         c.x = i*12;
         c.y = 36;
         c.color = 32;
+        c.pop = false;
         
         rowA[i] = a;
         rowB[i] = b;
@@ -65,8 +76,9 @@ int main(){
         else if(Buttons::rightBtn()) barX += speed;
         
         
-        
+        // Update balloon movement and player collision and draw
         for(int i = 0; i < rowSize; ++i){
+            
             rowA[i].x--;
             if(rowA[i].x < -8)rowA[i].x = Display::width;
             
@@ -76,13 +88,40 @@ int main(){
             rowC[i].x--;
             if(rowC[i].x < -8)rowC[i].x = Display::width;
             
+            if(y < 17 && !rowA[i].pop){
+               if (collides(x, y, rowA[i])){
+                    rowA[i].pop = true;
+                    score += 5;
+                    vy = 2;
+               }
+            }
             
-            Display::drawSprite(rowA[i].x, rowA[i].y, Balloon, false, false, rowA[i].color);
-            Display::drawSprite(rowB[i].x, rowB[i].y, Balloon, false, false, rowB[i].color);
-            Display::drawSprite(rowC[i].x, rowC[i].y, Balloon, false, false, rowC[i].color);
+            if(y < 27 && !rowB[i].pop){
+                if(collides(x, y, rowB[i])){
+                    rowB[i].pop = true;
+                    score += 5;
+                    vy = 2;
+                }
+            }
+            
+            if(y < 37 && !rowC[i].pop){
+                if(collides(x, y, rowC[i])){
+                    rowC[i].pop = true;
+                    score += 5;
+                    vy = 2;
+                }
+            }
+            
+            if(!rowA[i].pop) Display::drawSprite(rowA[i].x, rowA[i].y, Balloon, false, false, rowA[i].color);
+            if(!rowB[i].pop) Display::drawSprite(rowB[i].x, rowB[i].y, Balloon, false, false, rowB[i].color);
+            if(!rowC[i].pop) Display::drawSprite(rowC[i].x, rowC[i].y, Balloon, false, false, rowC[i].color);
         }
         
         
+        // Balloon collision
+        
+        
+        // Update player position
         if(y < 4) vy = 2;
         
         if(y >= barY-16) {
@@ -102,7 +141,7 @@ int main(){
         
         
         
-
+        //Draw player and bar
         Display::drawSprite(x, y, Smile);
         Display::drawSprite(x2, y2, Smile);
         Display::drawSprite(barX, barY, Bar, false, barMirror);
